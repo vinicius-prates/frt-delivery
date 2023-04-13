956f86c6ccc2bb2d95cb72608e00d9a7a8d4d0bd
vinicius-prates <viniprates2211@gmail.com>
2023-04-13 09:07:41 -0300

    feat(prisma): setup the data base

---
+++ server/prisma/migrations/20230413120643_first_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "fruit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "priceUnit" REAL NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
