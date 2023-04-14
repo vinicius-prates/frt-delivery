bd4be8d879cd0605e3e1d0a9381a594ba4017d9a
vinicius-prates <viniprates2211@gmail.com>
2023-04-14 11:16:33 -0300

    feat(procedure):  basic procedure

--- server/routers/product.ts
+++ server/routers/product.ts
@@ -1,5 +1,7 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson"
+import { z } from "zod";
+import { prisma } from "../src/utils/prisma";
 const t = initTRPC.create({
     transformer: superjson,
     errorFormatter({ shape }){
@@ -8,5 +10,30 @@ const t = initTRPC.create({
 })
 
 export const productRouter = t.router({
-    
+    create: t.procedure.input(z.object({
+        name: z.string(),
+        priceUnit: z.number(),
+        categoryName: z.string(),
+        description: z.string()
+    })).mutation(async ({input}) => {
+        try {
+            const product = prisma.product.create({
+                data:{
+                    name: input.name, 
+                    priceUnit: input.priceUnit,
+                    description: input.description,
+                    categoryName: input.categoryName 
+                }
+            })
+
+            return {product };
+        } catch (error) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            cause:error,
+            message:"Could not crate new product"
+          })  
+        }
+    }),
+
 })
